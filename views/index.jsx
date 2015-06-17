02be315d1e0854c01673498bdbb6fe24d26c2130
Gareth <me@gareth.codes>
2015-06-16 20:54:59 -0700

    added displayName and propTypes

--- views/index.jsx
+++ views/index.jsx
@@ -1,6 +1,10 @@
 var React = require('react')
 
 var Hi = React.createClass({
+  displayName: 'Hi',
+  propTypes: {
+    name: React.PropTypes.string.isRequired
+  },
   render: function () {
     return <div>Hello {this.props.name}</div>
   }
